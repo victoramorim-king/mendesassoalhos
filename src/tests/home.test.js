a1258061249a92017040b1b49bd67cba08501bd3
Victor Amorim <victoramorimchagas@gmail.com>
2022-12-09 17:32:00 -0300

    create new test files

---
+++ src/tests/home.test.js
@@ -0,0 +1,8 @@
+const home = require('../../public/js/home') 
+
+test('Add new item', () => {
+  expect(home(1, 2)).toBe(3);
+});
+
+
+
--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,4 +1,4 @@
-const server = require('./server');
+const server = require('../server');
 const calc = new server();
 
 
