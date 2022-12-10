82f4c1b03a08eb5f4217268900d105947e2362d6
Victor Amorim <victoramorimchagas@gmail.com>
2022-12-10 16:38:25 -0300

    Use the input data as global data.

--- src/tests/home.test.js
+++ src/tests/home.test.js
@@ -1,33 +1,43 @@
 const puppeteer = require('puppeteer');
 
+const headerInputsData = [
+  {'id':'consultor', 'value': 'Lucas Mendes'},
+  {'id':'cidade', 'value':'SP'},
+  {'id':'cliente', 'value':'Roberta Matarazzo'},
+  {'id':'logradouro', 'value': 'R. Marcos Mélega'},
+  {'id':'numero', 'value': '1234'},
+  {'id':'bairro', 'value': 'Alto de Pinheiros'},
+  {'id':'complemento', 'value': 'Apartamento 23B'},
+  {'id':'data', 'value': '10/12/2022'}
+]
+
+const headerInputsDataIncomplete = [
+  {'id':'consultor', 'value': 'Lucas Mendes'},
+  {'id':'cidade', 'value':'SP'},
+  {'id':'cliente', 'value':'Roberta Matarazzo'},
+  {'id':'logradouro', 'value': 'R. Marcos Mélega'},
+  {'id':'numero', 'value': '1234'},
+  {'id':'bairro', 'value': 'Alto de Pinheiros'},
+  {'id':'complemento', 'value': 'Apartamento 23B'},
+]
+
+const newItemInputsData = [
+  {'id':'quantidade', 'value': '19'},
+  {'id':'unidade', 'value':'m²'},
+  {'id':'descricao', 'value':'[Restauração completa] Raspagem, calafetação, polimento, aplicação de seladora e Bona Traffic'},
+  {'id':'valorUnitario', 'value': '123,33'},
+]
 
 test('Check if generate pdf button is avaliable with satisfied prerequisies', async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
   
-  const headerInputsData = [
-    {'id':'consultor', 'value': 'Lucas Mendes'},
-    {'id':'cidade', 'value':'SP'},
-    {'id':'cliente', 'value':'Roberta Matarazzo'},
-    {'id':'logradouro', 'value': 'R. Marcos Mélega'},
-    {'id':'numero', 'value': '1234'},
-    {'id':'bairro', 'value': 'Alto de Pinheiros'},
-    {'id':'complemento', 'value': 'Apartamento 23B'},
-    {'id':'data', 'value': '10/12/2022'}
-  ]
 
   for (const [i, input] of headerInputsData.entries()) {
     await page.click(`input#${input['id']}`);
     await page.type(`input#${input['id']}`, input['value']);
   }
-
-  const newItemInputsData = [
-    {'id':'quantidade', 'value': '19'},
-    {'id':'unidade', 'value':'m²'},
-    {'id':'descricao', 'value':'[Restauração completa] Raspagem, calafetação, polimento, aplicação de seladora e Bona Traffic'},
-    {'id':'valorUnitario', 'value': '123,33'},
-  ]
 
   for (const [i, input] of newItemInputsData.entries()) {
     await page.click(`input#${input['id']}`);
@@ -49,27 +59,11 @@ test('Check if generate pdf button IS NOT avaliable without satisfied prerequisi
   await page.goto('http://localhost:3000');
   
   // Input Data is not in the array to force the fail
-  const headerInputsData = [
-    {'id':'consultor', 'value': 'Lucas Mendes'},
-    {'id':'cidade', 'value':'SP'},
-    {'id':'cliente', 'value':'Roberta Matarazzo'},
-    {'id':'logradouro', 'value': 'R. Marcos Mélega'},
-    {'id':'numero', 'value': '1234'},
-    {'id':'bairro', 'value': 'Alto de Pinheiros'},
-    {'id':'complemento', 'value': 'Apartamento 23B'},
-  ]
 
-  for (const [i, input] of headerInputsData.entries()) {
+  for (const [i, input] of headerInputsDataIncomplete.entries()) {
     await page.click(`input#${input['id']}`);
     await page.type(`input#${input['id']}`, input['value']);
   }
-
-  const newItemInputsData = [
-    {'id':'quantidade', 'value': '19'},
-    {'id':'unidade', 'value':'m²'},
-    {'id':'descricao', 'value':'[Restauração completa] Raspagem, calafetação, polimento, aplicação de seladora e Bona Traffic'},
-    {'id':'valorUnitario', 'value': '123,33'},
-  ]
 
   for (const [i, input] of newItemInputsData.entries()) {
     await page.click(`input#${input['id']}`);
